6dc0ce95e942511b0b333dd19d6bcceac3150953
hungbn <hungbn@natajsc.com.vn>
2019-01-11 17:08:52 +0700

    Update theme

--- wp/wp-blog-header.php
+++ wp/wp-blog-header.php
@@ -17,5 +17,5 @@ if ( !isset($wp_did_header) ) {
 
 	// Load the theme template.
 	require_once( ABSPATH . WPINC . '/template-loader.php' );
-
+	
 }
--- wp/wp-content/themes/learning/index.php
+++ wp/wp-content/themes/learning/index.php
@@ -4,8 +4,23 @@ get_header();
 
 if (have_posts()): while (have_posts()): the_post();
 ?>
-  <h1><?= the_title() ?></h1>
-
+  <h2>
+    <a href="<?= get_permalink() ?>" title="<?= the_title_attribute() ?>"><?= get_the_title() ?></a>
+  </h2>
+  <div>
+    Posted on
+    <a href="<?= get_permalink() ?>">
+      <time datetime="<?= get_the_date('c') ?>"><?= get_the_date() ?></time>
+    </a>
+    By <a href="<?= get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>"><?= get_the_author() ?></a>
+  </div>
+  <div>
+    <?php the_excerpt() ?>
+  </div>
+  <a href="<?= get_permalink() ?>" title="<?= the_title_attribute() ?>">
+  Read more
+  </a>
+  <?php the_posts_pagination(); ?>
   <?php
 endwhile; else:
   echo "<h1>There is no post</h1>";
